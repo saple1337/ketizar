15ae8a28b5dc0945239cb0f36076dddb2f426629
Mio Akiyama <74380054+MioRevy@users.noreply.github.com>
2025-07-27 18:32:15 -0400

    navy fixed
    This reverts commit 6f227d323518313a2db4bf186420d21e8bd7bbe5.

--- common/defines/sr_defines.lua
+++ common/defines/sr_defines.lua
@@ -256,7 +256,7 @@ NDefines.NAir.COMBAT_DAMAGE_SCALE = 0.1-- (0.1)
 NDefines.NAir.COMBAT_DAMAGE_SCALE_CARRIER = 0.05
 
 NDefines.NAir.NAVAL_KAMIKAZE_DAMAGE_MULT = 5.0  -- vanilla is like 20
-NDefines.NAir.AA_INDUSTRY_AIR_DAMAGE_FACTOR = -0.2 -- -0.12 vanilla, per level AA state 
+NDefines.NAir.AA_INDUSTRY_AIR_DAMAGE_FACTOR = -0.12 -- -0.12 vanilla, per level AA state 
 --NDefines.NAir.ANTI_AIR_MAXIMUM_DAMAGE_REDUCTION_FACTOR = 0.68 -- .75 Maximum damage reduction factor applied to incoming air attacks against units with AA.
 
 NDefines.NAir.AIR_WING_XP_LOSS_WHEN_KILLED = 0.4
